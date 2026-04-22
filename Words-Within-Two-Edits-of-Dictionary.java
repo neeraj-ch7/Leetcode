e31de1b34dec350c379771c94f082a9f3c11f9b9
Neeraj Chauhan <ncs7704@gmail.com>
2026-04-22 22:21:15 +0530

    [2452] [Time Beats: 97.79%] [Memory Beats: 38.97%] - LeetPush

--- Words-Within-Two-Edits-of-Dictionary.java
+++ Words-Within-Two-Edits-of-Dictionary.java
@@ -1,29 +1,30 @@
-1class Solution {
-2    public List<String> twoEditWords(String[] queries, String[] dictionary) {
-3        List<String> result = new ArrayList<>();
-4
-5        for (String q : queries) {
-6            for (String d : dictionary) {
-7                if (isWithinTwoEdits(q, d)) {
-8                    result.add(q);
-9                    break; // no need to check further
-10                }
-11            }
-12        }
-13
-14        return result;
-15    }
-16
-17    private boolean isWithinTwoEdits(String a, String b) {
-18        int diff = 0;
-19
-20        for (int i = 0; i < a.length(); i++) {
-21            if (a.charAt(i) != b.charAt(i)) {
-22                diff++;
-23                if (diff > 2) return false;
-24            }
-25        }
-26
-27        return true;
-28    }
-29}
+1
+2 class Solution {
+3    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+4        List<String> result = new ArrayList<>();
+5
+6        for (String q : queries) {
+7            for (String d : dictionary) {
+8                if (isWithinTwoEdits(q, d)) {
+9                    result.add(q);
+10                    break; // no need to check further
+11                }
+12            }
+13        }
+14
+15        return result;
+16    }
+17
+18    private boolean isWithinTwoEdits(String a, String b) {
+19        int diff = 0;
+20
+21        for (int i = 0; i < a.length(); i++) {
+22            if (a.charAt(i) != b.charAt(i)) {
+23                diff++;
+24                if (diff > 2) return false;
+25            }
+26        }
+27
+28        return true;
+29    }
+30}
