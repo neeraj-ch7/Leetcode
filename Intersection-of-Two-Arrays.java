74180993cfb6d571d1290d4a3e1f29866e6467ff
Neeraj Chauhan <ncs7704@gmail.com>
2026-05-09 22:17:08 +0530

    [349] [Time Beats: 34.20%] [Memory Beats: 6.36%] - LeetPush

--- Intersection-of-Two-Arrays.java
+++ Intersection-of-Two-Arrays.java
@@ -1,24 +1,22 @@
-1import java.util.*;
-2
-3class Solution {
-4    public int[] intersection(int[] nums1, int[] nums2) {
-5        
-6        Set<Integer> set2 = new HashSet<>();
-7        Set<Integer> result = new HashSet<>();
-8        
-9        // nums2 ko set me convert karo
-10        for (int num : nums2) {
-11            set2.add(num);
-12        }
-13        
-14        // check intersection
-15        for (int num : nums1) {
-16            if (set2.contains(num)) {
-17                result.add(num);
-18            }
-19        }
-20        
-21        // convert to array
-22        return result.stream().mapToInt(Integer::intValue).toArray();
-23    }
-24}
+1class Solution {
+2    public int[] intersection(int[] nums1, int[] nums2) {
+3        
+4        Set<Integer> set2 = new HashSet<>();
+5        Set<Integer> result = new HashSet<>();
+6        
+7        // nums2 ko set me convert karo
+8        for (int num : nums2) {
+9            set2.add(num);
+10        }
+11        
+12        // check intersection
+13        for (int num : nums1) {
+14            if (set2.contains(num)) {
+15                result.add(num);
+16            }
+17        }
+18        
+19        // convert to array
+20        return result.stream().mapToInt(Integer::intValue).toArray();
+21    }
+22}
