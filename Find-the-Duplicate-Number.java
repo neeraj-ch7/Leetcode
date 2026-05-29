cfbbecb7bf60e793ee195b598dd2763d8c932b02
Neeraj Chauhan <ncs7704@gmail.com>
2026-05-29 13:00:23 +0530

    [287] [Time Beats: 91.74%] [Memory Beats: 26.37%] - LeetPush

--- Find-the-Duplicate-Number.java
+++ Find-the-Duplicate-Number.java
@@ -1,25 +1,23 @@
-1import java.util.*;
-2
-3class Solution {
-4    public int findDuplicate(int[] nums) {
-5        
-6      int slow=0;
-7      int fast =0;
-8
-9      while(true){
-10         slow=nums[slow];
+1
+2class Solution {
+3    public int findDuplicate(int[] nums) {
+4        
+5      int slow=0;
+6      int fast =0;
+7
+8      while(true){
+9         slow=nums[slow];
+10         fast=nums[fast];
 11         fast=nums[fast];
-12         fast=nums[fast];
-13         if(slow==fast){
-14            slow=0;
-15         while(slow!=fast){
-16            slow=nums[slow];
-17            fast=nums[fast];
-18           }
-19           return slow;
-20         }
-21        
-22      }
-23
-24    }
-25}
+12         if(slow==fast){
+13            slow=0;
+14         while(slow!=fast){
+15            slow=nums[slow];
+16            fast=nums[fast];
+17           }
+18           return slow;
+19         }
+20        
+21      }
+22    }
+23}
