562cf6ceb6fe607ea63bf1244d4d2e4286bc65f2
Neeraj Chauhan <ncs7704@gmail.com>
2026-08-02 22:10:33 +0530

    [877] [Time Beats: 5.73%] [Memory Beats: 5.73%] - LeetPush

---
+++ 877-Stone-Game.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    Integer[][] dp;
+
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        dp = new Integer[n][n];
+
+        return solve(piles, 0, n - 1) > 0;
+    }
+
+    private int solve(int[] piles, int i, int j) {
+
+        if (i == j)
+            return piles[i];
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        int takeLeft = piles[i] - solve(piles, i + 1, j);
+        int takeRight = piles[j] - solve(piles, i, j - 1);
+
+        return dp[i][j] = Math.max(takeLeft, takeRight);
+    }
+}
