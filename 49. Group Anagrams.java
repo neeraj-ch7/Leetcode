a5d00fc44b0e7844b0017950488e086539ee77d9
Neeraj Chauhan <ncs7704@gmail.com>
2026-09-01 21:42:08 +0530

    Implement groupAnagrams method to group anagrams

---
+++ 49. Group Anagrams.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+           
+            char[] chars = str.toCharArray();
+
+           
+            Arrays.sort(chars);
+
+          
+            String key = new String(chars);
+
+            
+            map.computeIfAbsent(key, k -> new ArrayList<>())
+               .add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
